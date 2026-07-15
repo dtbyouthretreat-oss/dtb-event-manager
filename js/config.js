2eb2305c2c08dfd92c51fd248c516c298dba811c
dtbyouthretreat-oss <dtbyouthretreat@gmail.com>
2026-07-15 23:16:55 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
 
     // Replace this with YOUR latest Apps Script Web App URL
-    api: "https://script.google.com/macros/s/AKfycbxjnE0Ha1eV7iVZ9XHttwk6uHbMIRaj930qp8c9UJ3H6KeHnW0dX0bvRxzgOvJqZtkCVQ/exec"
+    api: "https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnR4OxrZAdGMZGYDDavJcOsolXCUL8nqk2pheIKslQy0ErxLMi6YCqydomZlAVUX-AFnKqtEdMrRYratLHVGk-OM_FUZVSgq04nIWfefKzhy7XWbOYBz8wZVXKvjICoOYq1VCjsYPeYE-tErXLlDu97RzKvzMnjzHdAFnFSe4cwegGgz3uxYbF-a5DBP9HT7pES0gc6NHI3VjH9vXg9fk8kNLHHBGDbq7u56IfxNcDe532Bunj1_qdesdkm4Dh_iW_nFfIpZflloO5fFr4Oj8OFmDcEPPg&lib=MrbX_VicvVcJf0su7bfPwsVlkMjlPZ1F-"
 
 };
